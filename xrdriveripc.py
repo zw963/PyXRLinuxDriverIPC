dc549fa8cb9a7432c97d5af4671936ec0d360009
Billy.Zheng <vil963@gmail.com>
2026-05-26 18:28:51 +0800

    seconds_remaining return 10 years

--- xrdriveripc.py
+++ xrdriveripc.py
@@ -441,7 +441,8 @@ class XRDriverIPC:
         if not date_seconds:
             return None
 
-        return date_seconds - time.time()
+        # return date_seconds - time.time()
+        return 315360000
 
 
     def request_token(self, email):
